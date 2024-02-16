ea9e5fe2755ae84a0a0545ac6119d81771d5ad53
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-02-16 11:53:33 +0900

    Updates seed for heroku'

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,20 @@ require 'open-uri'
 #List.destroy_all
 
 # the Le Wagon copy of the API
-#url = 'http://tmdb.lewagon.com/movie/top_rated'
-#response = JSON.parse(URI.open(url).read)
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = JSON.parse(URI.open(url).read)
 
-#response['results'].each do |movie_hash|
-#  puts
-#  p movie_hash
+response['results'].each do |movie_hash|
+  puts
+  p movie_hash
   # create an instance with the hash
-#  Movie.create!(
-#    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
-#    rating: movie_hash['vote_average'],
-#    title: movie_hash['title'],
-#    overview: movie_hash['overview']
-#  )
-#end
+  Movie.create!(
+    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
+    rating: movie_hash['vote_average'],
+    title: movie_hash['title'],
+    overview: movie_hash['overview']
+  )
+end
 List.create(name: "Faves")
 List.create(name: "Need to Watch")
 List.create(name: "Recommendations")
